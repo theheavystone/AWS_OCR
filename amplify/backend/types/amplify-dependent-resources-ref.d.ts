9db7f40388c00826f7d911eccdeaeeb0e41c05b7
ZHANGLUWEI <zhangluwei@ZHANGLUWEIdeMacBook-Pro.local>
2025-04-10 19:12:20 +0900

    S3

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ocrwebapp817d4dcf": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "ocrImageStorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
